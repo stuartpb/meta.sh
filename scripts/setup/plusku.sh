35cf516e7cc08e18c086615ba62e8356c002d127
Stuart P. Bentley <stuart@testtrack4.com>
2014-08-18 01:55:48 -0700

    Unify plusku setup scripts

---
+++ scripts/setup/plusku.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+if [[ $EUID != 0 ]]; then
+  echo "You need to be root to install Plusku" >&2
+  exit 1
+fi
+
+# Determine distro
+distro=`[[ -f /etc/os-release ]] &&
+  sed -n 's/^ID=\(.*\)$/\1/p' /etc/os-release`
+
+case "$distro" in
+  arch)
+    # update the system
+    pacman -Syu --noconfirm
+
+    # Add dependencies
+    pacman -S --noconfirm --needed sudo git docker nginx
+
+    # Set up and start services
+    systemctl enable docker nginx
+    systemctl start docker nginx
+    ;;
+
+  ubuntu)
+    # Get Docker
+    curl -s https://get.docker.io/ubuntu/ | sh
+
+    # Add Nginx PPA
+    add-apt-repository -y ppa:nginx/stable
+
+    # Update apt-get lists
+    apt-get update
+
+    # Install other dependencies
+    apt-get install -y git nginx dnsutils
+    ;;
+  *)
+    echo "This setup script does not support this distro." >&2
+    exit 1
+    ;;
+esac
+
+# Install Plushu
+git clone https://github.com/plushu/plushu /home/plushu
+/home/plushu/install.sh
+
+# Install Plusku
+plushu plugins:install plusku
